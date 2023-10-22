543a2fabe9dfed7e023724d288ecb14b5e02a7ec
Omer Erbalta <erbalta80@gmail.com>
2023-10-22 09:34:52 +0300

    Initial Commit

---
+++ InstaProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstaProject
+//
+//  Created by OmerErbalta on 22.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstaProject/InstaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaProjectApp.swift
+//  InstaProject
+//
+//  Created by OmerErbalta on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
