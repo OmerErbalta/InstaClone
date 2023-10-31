bdb1d1ebe840886349acdc3fdd72a29af7390484
Omer Erbalta <erbalta80@gmail.com>
2023-10-31 15:45:52 +0300

    add comment view

---
+++ InstaProject/Core/Feed/View/CommentBottomSheet.swift
@@ -0,0 +1,18 @@
+//
+//  CommentBottomSheet.swift
+//  InstaProject
+//
+//  Created by OmerErbalta on 31.10.2023.
+//
+
+import SwiftUI
+
+struct CommentBottomSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentBottomSheet()
+}
