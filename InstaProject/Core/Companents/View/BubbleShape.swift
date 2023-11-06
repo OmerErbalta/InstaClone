fee19080196a45aaccbe7429e23c7a670ecc96d8
Omer Erbalta <erbalta80@gmail.com>
2023-11-06 19:45:44 +0300

    added Chat View

---
+++ InstaProject/Core/Companents/View/BubbleShape.swift
@@ -0,0 +1,8 @@
+//
+//  BubbleShape.swift
+//  InstaProject
+//
+//  Created by OmerErbalta on 6.11.2023.
+//
+
+import Foundation
---
+++ InstaProject/Core/Companents/View/ChatMessage.swift
@@ -0,0 +1,18 @@
+//
+//  ChatMessage.swift
+//  InstaProject
+//
+//  Created by OmerErbalta on 6.11.2023.
+//
+
+import SwiftUI
+
+struct ChatMessage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatMessage()
+}
