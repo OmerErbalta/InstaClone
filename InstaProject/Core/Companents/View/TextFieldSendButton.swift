6d512666e5288dc3d0820298c43d80981cf6f23e
Omer Erbalta <erbalta80@gmail.com>
2023-11-06 18:03:09 +0300

    added viewSwitch

--- InstaProject/Core/Companents/View/TextFieldSendButton.swift
+++ InstaProject/Core/Companents/View/TextFieldSendButton.swift
@@ -1,5 +1,5 @@
 //
-//  MessageBoxCell.swift
+//  TextFieldSendButton.swift
 //  InstaProject
 //
 //  Created by OmerErbalta on 6.11.2023.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct MessageBoxCell: View {
+struct TextFieldSendButton: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MessageBoxCell()
+    TextFieldSendButton()
 }
